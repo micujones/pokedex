ab13417b188efb3828b668dc4024735964c0a2e6
Mike Jones, II <micujonesii@gmail.com>
2024-10-08 14:00:48 -0400

    Create an array list of pokémon objects

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# pokedex
+# Pokédex
+
+## Project description
+
+The objective is create an online encyclopedia of Pokémon, also known as a pokédex, which includes an easily navigatable UI and an API from which to load information about each pokémon. This project is the culmination of a series of exercises from CareerFoundry's Full-Stack Web Development course.
+
+## Getting the Pokédex runnning
+
+## Project dependencies
+
+## APIs
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,41 @@
-alert('hello world');
+let pokemonList = [
+    {
+        name: 'Charmander',
+        height: 2,
+        types: ['fire'],
+        weaknesses: ['water', 'ground', 'rock']
+    },
+    {
+        name: 'Cyndaquil',
+        height: 2,
+        types: ['fire'],
+        weaknesses: ['water', 'ground', 'rock']
+    },
+    {
+        name: 'Dragonite',
+        height: 7,
+        types: ['dragon', 'flying'],
+        weaknesses: ['ice', 'rock', 'dragon', 'fairy']
+    },
+    {
+        name: 'Gengar',
+        height: 5,
+        types: ['ghost', 'poison'],
+        weaknesses: ['ground', 'psychic', 'ghost', 'dark']
+    },
+    {
+        name: 'Machop',
+        height: 3,
+        types: ['fighting'],
+        weaknesses: ['flying', 'psychic', 'fairy']
+    },
+    {
+        name: 'Mewtwo',
+        height: 6,
+        types: ['psychic'],
+        weaknesses: ['bug', 'ghost', 'dark']
+    }
+];
 
-let favoriteFood = 'Nanab Berry';
-
-document.write(favoriteFood);
+alert(pokemonList[3].name);
+document.write(pokemonList[3].name);
