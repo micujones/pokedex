4c556cf9e32ea728f902ff96e88a358e151c21f2
Mike Jones, II <micujonesii@gmail.com>
2024-10-08 22:37:47 -0400

    Initialize IIFE for pokémon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,64 @@
-let pokemonList = [
-    {
-        name: 'Charmander',
-        height: 2,
-        types: ['fire'],
-        weaknesses: ['water', 'ground', 'rock']
-    },
-    {
-        name: 'Cyndaquil',
-        height: 2,
-        types: ['fire'],
-        weaknesses: ['water', 'ground', 'rock']
-    },
-    {
-        name: 'Dragonite',
-        height: 7,
-        types: ['dragon', 'flying'],
-        weaknesses: ['ice', 'rock', 'dragon', 'fairy']
-    },
-    {
-        name: 'Gengar',
-        height: 5,
-        types: ['ghost', 'poison'],
-        weaknesses: ['ground', 'psychic', 'ghost', 'dark']
-    },
-    {
-        name: 'Machop',
-        height: 3,
-        types: ['fighting'],
-        weaknesses: ['flying', 'psychic', 'fairy']
-    },
-    {
-        name: 'Mewtwo',
-        height: 6,
-        types: ['psychic'],
-        weaknesses: ['bug', 'ghost', 'dark']
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {
+            name: 'Charmander',
+            height: 2,
+            types: ['fire'],
+            weaknesses: ['water', 'ground', 'rock']
+        },
+        {
+            name: 'Cyndaquil',
+            height: 2,
+            types: ['fire'],
+            weaknesses: ['water', 'ground', 'rock']
+        },
+        {
+            name: 'Dragonite',
+            height: 7,
+            types: ['dragon', 'flying'],
+            weaknesses: ['ice', 'rock', 'dragon', 'fairy']
+        },
+        {
+            name: 'Gengar',
+            height: 5,
+            types: ['ghost', 'poison'],
+            weaknesses: ['ground', 'psychic', 'ghost', 'dark']
+        },
+        {
+            name: 'Machop',
+            height: 3,
+            types: ['fighting'],
+            weaknesses: ['flying', 'psychic', 'fairy']
+        },
+        {
+            name: 'Mewtwo',
+            height: 6,
+            types: ['psychic'],
+            weaknesses: ['bug', 'ghost', 'dark']
+        }
+    ];
+
+    function getAll() {
+        return pokemonList;
     }
-];
 
-alert(`There are ${pokemonList.length} pokémon going to be printed.`);
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
 
-for (i = 0; i < pokemonList.length; i++) {
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
+
+alert(`There are ${pokemonRepository.getAll().length} pokémon going to be printed.`);
+
+pokemonRepository.getAll().forEach(pokemon => {
     document.write('<p>');
-    document.write(`${pokemonList[i].name} (Height: ${pokemonList[i].height}\'\')`);
-    if (pokemonList[i].height >= 5) {
+    document.write(`${pokemon.name} (Height: ${pokemon.height}\'\')`);
+    if (pokemon.height >= 5) {
         document.write(' ... that\'s big!');
     }
     document.write('</p>');
-}
+});
